cffb47e616c2cd978a9a8ee7f8d75eb561e2c3f3
吴天翔 <wutianxiang@uniontech.com>
2020-05-24 14:10:46 +0800

    feat: add cpu file

---
+++ cpu/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"runtime/pprof"
+)
+
+// CPU性能分析
+func main() {
+	var isCPUPprof bool
+	flag.BoolVar(&isCPUPprof, "cpu", false, "turn cpu pprof on")
+	flag.Parse()
+
+	if isCPUPprof {
+		file, err := os.Create("./cpu.pprof")
+		defer file.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(file)
+		defer pprof.StopCPUProfile()
+	}
+
+	joinSlice()
+}
+
+// 一段性能问题代码
+func joinSlice() []int {
+	//var slices []int
+	num := 10000000
+	slices := make([]int, 0, num)
+	for i := 0; i < num; i++ {
+		// 故意造成多次切片添加，由于每次操作可能会有内存重新分配和移动，性能较低
+		slices = append(slices, i+1)
+	}
+	return slices
+}
